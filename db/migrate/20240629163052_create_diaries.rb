4fff06695854cb6f089e11b41297d8ba26527b5c
kapi1121 <157882257+kapi1121@users.noreply.github.com>
2024-06-30 01:52:42 +0900

    マイグレーションファイルの設定

--- db/migrate/20240629163052_create_diaries.rb
+++ db/migrate/20240629163052_create_diaries.rb
@@ -2,8 +2,8 @@ class CreateDiaries < ActiveRecord::Migration[7.0]
   def change
     create_table :diaries do |t|
       t.references :baby, null: false, foreign_key: true
-      t.date :entry_date
-      t.text :content
+      t.date :entry_date, null: false
+      t.text :content,    null: false
       t.string :photo
 
       t.timestamps
--- db/migrate/20240629163103_create_advices.rb
+++ db/migrate/20240629163103_create_advices.rb
@@ -1,8 +1,8 @@
 class CreateAdvices < ActiveRecord::Migration[7.0]
   def change
     create_table :advices do |t|
-      t.integer :age_months
-      t.text :advice
+      t.integer :age_months, null: false
+      t.text :advice,        null: false
 
       t.timestamps
     end
